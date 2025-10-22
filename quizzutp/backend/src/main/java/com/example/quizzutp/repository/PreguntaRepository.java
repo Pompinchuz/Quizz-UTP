df04084aac8afcffe6035254ab25385a31f68fea
Pompinchuz <huillajesusantonio@gmail.com>
2025-10-22 00:40:07 -0500

    V03 - controller

--- quizzutp/backend/src/main/java/com/example/quizzutp/repository/PreguntaRepository.java
+++
@@ -1,7 +0,0 @@
-// PreguntaRepository.java
-package com.quizzutp.backend.repository;
-
-import com.quizzutp.backend.model.Pregunta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PreguntaRepository extends JpaRepository<Pregunta, Long> {}
